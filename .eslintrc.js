996fcb17472d54d84ab9abc3d0743f5e663dbbb6
Peter Shoukry <pshoukry@gmail.com>
2018-05-14 21:37:33 +0200

    Exporting all lib imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,17 @@ module.exports = {
   "env": {
     "es6": true,
     "node": true
-
   },
   "extends": "google",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+  },
   "rules": {
     "semi": [
       "error",
       "never"
     ]
   }
-};
+}
 
