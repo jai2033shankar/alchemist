75c232c5d37058860ecfe55b61aba6403806eb4b
Peter Shoukry <pshoukry@gmail.com>
2018-07-09 15:51:22 +0200

    Add csv output

---
+++ src/pipeline/adapters/s3_csv_output.js
@@ -0,0 +1,25 @@
+import promisify from 'util.promisify'
+import AWS from 'aws-sdk'
+
+var S3 = new AWS.S3()
+
+export default class S3FileOutput {
+  constructor(opts) {
+    this.params = {
+      Bucket: opts.bucket,
+      Key: opts.key,
+    }
+  }
+
+  async write(pipelineData) {
+    let body = await pipelineData.getData()
+    let items = body.items
+    let replacer = (key, value) => value === null ? '' : value
+    let header = Object.keys(items[0])
+    let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+    csv.unshift(header.join(','))
+    csv = csv.join('\r\n')
+    await S3.putObject(Object.assign(this.params, {Body: csv})).promise()
+  }
+}
+
