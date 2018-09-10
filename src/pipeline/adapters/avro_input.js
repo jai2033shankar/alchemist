7ec4c781e7402ea8d1b242012569e02109339804
John Donner <johnbdonner@gmail.com>
2018-09-10 17:40:19 -0400

    [FIX] fixed up file

--- src/pipeline/adapters/avro_input.js
+++ src/pipeline/adapters/avro_input.js
@@ -2,28 +2,22 @@ import promisify from 'util.promisify'
 import avro from 'avro-js'
 import fs from 'fs'
 
-var S3 = new AWS.S3()
-
 export default class AvroInput {
   constructor(opts) {
-    this.avroFilePath = path.resolve(__dirname, opts.avroFilePath)
+    this.avroFilePath = opts.avroFilePath
   }
 
   async read(pipelineData) {
     await this.loadFile()
 
-    let avroData = {}
+    this.avroFile.pipe(new avro.streams.BlockDecoder())
+      .on('data', async (record) => pipelineData.putData(record))
 
-    this.file.pipe(new avro.streams.BlockDecoder())
-      .on('metadata', async (type) => avroData.type = type)
-      .on('data', async (record) => avroData.record = record)
-
-    pipelineData.putData(avroData)
     return pipelineData
   }
 
   async loadFile() {
-    this.file = await fs.createReadStream(this.avroFilePath)
+    this.avroFile = await fs.createReadStream(this.avroFilePath)
   }
 }
 
